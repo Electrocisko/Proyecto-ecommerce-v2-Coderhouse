0eee2cd37345ab0ba67cd23e24d969290f2e54c6
Francisco Zuidwijk <electrocisko@gmail.com>
2022-11-21 15:44:20 -0300

    testing

--- src/appi.test.js
+++ src/appi.test.js
@@ -21,38 +21,59 @@ describe("Products Testing", () => {
     });
   });
   describe("PUTS", () => {
-    it("modify product by Id", async () => {
+    it("modify product price by Id", async () => {
       let product = {
         price: randomPrice,
         stock: "4",
       };
-      const response = await requester.put("/api/products/637b6a6d909be982a42b673c").send(product)
-      const { _body } = response
-      expect(_body.status.modifiedCount).to.be.equal(1)
+      const response = await requester
+        .put("/api/products/637b6a6d909be982a42b673c")
+        .send(product);
+      const { _body } = response;
+      expect(_body.status.modifiedCount).to.be.equal(1);
     });
   });
 });
 
-describe('UserTesting', () => {
-    describe('GETS', () => {
-        it('Return 200', async () => {
-            let response = await requester.get('/api/users')
-            const { _body} = response
-            expect(response.status).to.be.equals(200);
-        })
-    })
-})
+describe("User Testing", () => {
+  describe("GETS", () => {
+    it("Return 200", async () => {
+      let response = await requester.get("/api/users");
+      const { _body } = response;
+      expect(response.status).to.be.equals(200);
+    });
+  });
+});
 
-
-describe('SessionTesting', () => {
-    describe('GETS', () => {
-        it('Return 200', async () => {
-            let response = await requester.get('/api/users')
-            const { _body} = response
-            expect(response.status).to.be.equals(200);
-        })
-    })
-})
-
-
-
+describe("Session Testing", () => {
+  describe("POST", () => {
+    it("login test with an existing user", async () => {
+      let user = {
+        email: "chicacoder@correo.com",
+        password: "123",
+      };
+      let response = await requester.post("/api/sessions/login").send(user);
+      expect(response.status).to.be.equals(200);
+    });
+  });
+  describe("POST", () => {
+    it("login test with an non existing user", async () => {
+      let user = {
+        email: "noexisto@correo.com",
+        password: "123",
+      };
+      let response = await requester.post("/api/sessions/login").send(user);
+      expect(response.status).to.be.not.equal(200);
+    });
+  });
+  describe("POST", () => {
+    it("register a new user", async () => {
+      let user = {
+        email: "noexisto@correo.com",
+        password: "123",
+      };
+      let response = await requester.post("/api/sessions/login").send(user);
+      expect(response.status).to.be.not.equal(200);
+    });
+  });
+});
